46c85a91b9df4bf97c43efb363306035f2f2f528
Oliver lindenskov <Oliver.lindenskov157@gmail.com>
2016-04-08 15:25:31 +0200

    Test for changing model to work with food

--- models/posts.js
+++ models/posts.js
@@ -2,19 +2,29 @@ var mongoose = require('mongoose');
 
 // Create the MovieSchema.
 var PostsSchema = new mongoose.Schema({
+  user_id: {type: String , required: true},
+  type: {type: String , required: true},
   picture: {type: String , required: true},
-  userid: {type: String, required: true},
-  age: {type: String , required: false},
-  size: {type : String , required : true},
-  colour: {type : String , required : true},
-  kind: {type : String , required : true},
-  state: {type : String , required : true},
-  inret: {type : String , required : true},
-  male: {type : Boolean , required : true},
-  about: {type : String , required : true},
-  latitude: {type : Number , required : true},
-  longitude: {type : Number , required : true},
-  likes: {type: Array, required : true}
+  about: {type: String , required: true},
+  upload: {type: String , required: true},
+  hosted: {type: String , required: true},
+  address: {type: String , required: true},
+  genre: {type: String , required: true},
+  dish: {type: String , required: true},
+  guests: {
+    forEat: {type: Number , required: true},
+    forDine: {type: String , required: true}
+  },
+  tags: {type: Array , required: true},
+  bringAlong: {type: Array , required: true},
+  total: {type: Number , required: true},
+  filled: {
+  	guests: {
+  		forEat: {type: Number , required: true},
+  		forDine: {type: Number , required: true}
+  	},	
+	  bringAlong: {type: String , required: true}
+  }
 });
 
 module.exports = mongoose.model('posts',PostsSchema);
